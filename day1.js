0dbd96245d1b2bdee4016ecd628acd748564b156
Ahmed Anwar <anwar069@gmail.com>
2021-08-09 00:55:36 +0530

    Day 2

---
+++ day1.js
@@ -0,0 +1,7 @@
+// Variable decalration 
+let a = 10;
+let b = 20;
+
+// Print sum of a and b
+console.log("a + b =", a+b);
+
---
+++ day2.js
@@ -0,0 +1,37 @@
+// let age=18;
+// let password = 'abd';
+// // { } -> Curly bracket / braces
+// // ( ) -> Parenthesis  / cicul;ar bracket 
+// // [ ] -> Square bracket 
+// if (age >= 18) {
+//     console.log('Voet kar skta hai');
+// }
+// // console.log('log 1',age,password);
+// // console.log('log 2',age,password);
+
+// // console.log('log 3',age,password);
+
+// // console.log('log 4',age,password);
+
+
+// Variable declaration
+let userName = 'Wajid';
+let area;
+
+
+// condtion check
+if (userName == 'Arfat') {
+    area='Mominpura';
+};
+
+if (userName == 'Tarique') {
+    area='Teka';
+};
+
+if (userName == 'Wajid') {
+    area='Takiya';
+};
+
+
+// Result
+console.log(userName,area,'me rehta hai');
