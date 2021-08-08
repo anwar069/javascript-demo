b100265b4f30da712b46d8ecf5817c10c1d64b59
Ahmed Anwar <anwar069@gmail.com>
2021-08-09 01:24:59 +0530

    Day 2

--- day2.js
+++ day2.js
@@ -14,24 +14,40 @@
 // // console.log('log 4',age,password);
 
 
-// Variable declaration
-let userName = 'Wajid';
-let area;
+// // Variable declaration
+// let userName = 'Wajid';
+// let area;
 
 
-// condtion check
-if (userName == 'Arfat') {
-    area='Mominpura';
-};
+// // condtion check
+// if (userName == 'Arfat') {
+//     area='Mominpura';
+// };
 
-if (userName == 'Tarique') {
-    area='Teka';
-};
+// if (userName == 'Tarique') {
+//     area='Teka';
+// };
 
-if (userName == 'Wajid') {
-    area='Takiya';
-};
+// if (userName == 'Wajid') {
+//     area='Takiya';
+// };
 
 
-// Result
-console.log(userName,area,'me rehta hai');
+// // Result
+// console.log(userName,area,'me rehta hai');
+
+let number = 2;
+let end = 10;
+
+// console.log("2 * 1 =",number*1);
+// console.log("2 * 2 =",number*2);
+// console.log("2 * 3 =",number*3);
+// console.log("2 * 4 =",number*4);
+// console.log("2 * 1 =",number*1);
+// console.log("2 * 2 =",number*2);
+// console.log("2 * 3 =",number*3);
+// console.log("2 * 4 =",number*4);
+
+for (let i = 1; i <= end; i++) {
+    console.log("2 * ",i , "=",number * i);
+}
