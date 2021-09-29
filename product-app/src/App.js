c62e13f3772359f901e2ef88ef85763a06b8db56
anwar069 <anwar069@gmail.com>
2021-09-30 00:32:33 +0530

    Added

--- product-app/src/App.js
+++ product-app/src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import PRODUCT_DATA from "./data/product-data.js"
-import { Navbar, Container, Row, Col, Form } from 'react-bootstrap';
+import { Navbar, Container, Row, Col, Form, Alert } from 'react-bootstrap';
 import ProductComponent from "./components/ProductComponent";
 import { useState } from 'react';
 
 function App() {
   const [searchVal, setSearchVal] = useState('');
   const [products, setProducts] = useState(PRODUCT_DATA);
+  const [cart, setCart] = useState([]);
 
   const handleChange = e => {
 
@@ -18,8 +19,21 @@ function App() {
       e => e.name.toLowerCase().includes(val.toLowerCase())
     )
     setProducts(filterProducts);
-
   }
+
+  const checkInCart = (pro) => {
+    if (pro.id === 1) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  const onAddCart = (item) => {
+    let arr = [item, ...cart];
+    setCart(arr);
+  }
+
   return (
     <>
       <Navbar bg="dark" variant="dark">
@@ -35,7 +49,6 @@ function App() {
                   className="d-inline-block align-top"
                 />{' '}
                 Taj Textiles Stores
-
               </Col>
 
             </Row>
@@ -47,17 +60,36 @@ function App() {
               value={searchVal}
               onChange={handleChange} />
           </Col>
+          <Col xs={1}>
+            <p className="cart"> Cart : {cart.length}</p>
+          </Col>
+          <p> </p>
         </Container>
       </Navbar>
       <Container fluid>
         <Row>
-          {products.map(
-            (item) => {
-              return (
-                <Col xs={12} md={3} ><ProductComponent product={item} /></Col>
-              )
-            }
-          )}
+          {products.length == 0 ?
+            <Alert variant="danger" >
+              <Alert.Heading>No Product found!</Alert.Heading>
+              <p>
+                Please change search text
+              </p>
+            </Alert>
+            : products.map(
+              (item) => {
+                return (
+                  <Col xs={12} md={3} >
+                    <ProductComponent
+                      product={item}
+                      onAddClick={onAddCart}
+                      isInCart={
+                        checkInCart(item)
+                      }
+                    />
+                  </Col>
+                )
+              }
+            )}
 
         </Row>
       </Container>
--- product-app/src/components/ProductComponent.js
+++ product-app/src/components/ProductComponent.js
@@ -4,14 +4,22 @@ function ProductComponent(props) {
     return (
         <>
             <Card className="product-card">
+
                 <Card.Header as="h5">{props.product.name}</Card.Header>
                 <Card.Body>
                     <Card.Title>Rs. {props.product.price}</Card.Title>
                     <Card.Text>
                         <p>Discount : {props.product.discount}</p>
                         <p>In Stock : {props.product.stock}</p>
+                        <p>In Cart : {props.isInCart.toString()}</p>
                     </Card.Text>
-                    <Button variant="danger">Add to cart</Button>
+                    {props.isInCart ? <Button
+                        variant="danger">Remove from cart</Button> : <Button
+                            onClick={() => { props.onAddClick(props.product) }}
+                            variant="info">Add to cart</Button>
+                    }
+
+
                 </Card.Body>
             </Card>
         </>
